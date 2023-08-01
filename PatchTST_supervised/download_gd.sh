91da38dc99acd7301b198936819e8b51b22231a0
nghiach <chnghia@gmail.com>
2023-08-01 15:43:48 +0700

    update with mlflow project

---
+++ PatchTST_supervised/download_gd.sh
@@ -0,0 +1,9 @@
+export fileid=1alE33S1GmP5wACMXaLu50rDIoVzBM4ik
+export filename=all_six_datasets.zip
+
+## WGET ##
+wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- \
+     | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
+
+wget --load-cookies cookies.txt -O $filename \
+     'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
